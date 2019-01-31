608a88253dd1bd496e834441e26de82badc7fbc1
Ilya <middls647@gmail.com>
2019-02-01 02:54:09 +0300

    Настройка import scss
    Изменили gulpfile, продолжили настройку среды

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var path = {
     },
     src: {
         html: 'src/*.html',
-        js: 'src/js/main.js',
+        js: 'src/js/link.js',
         style: 'src/style/main.scss',
         img: 'src/img/**/*.*',
         fonts: 'src/fonts/**/*.*'
